74bfb7d5170dad3bc2df84afcd1c052ccfb64093
Khanh Dao <khanhdao.knd@gmail.com>
2018-06-25 19:12:13 +0700

    Suppress error

--- app/index.ts
+++ app/index.ts
@@ -5,7 +5,7 @@ import Listr from 'listr';
 
 
 program
-    .version('0.5.1')
+    .version('0.5.2')
     .command('update <submodule>')
     .option('-C, --commitHash [commitHash]', 'commit id')
     .option('-b, --branch [updateSubmoduleBranch]', 'create update submodule branch from "develop" branch')
@@ -17,52 +17,47 @@ program
                 title: `Fetch ALL upstream`,
                 skip: () => false,
                 task: ctx => execa.shell(`git fetch upstream --recurse-submodules --prune`)
-                    .catch((err) => {
+                    .catch(() => {
                         ctx.abort = true;
-                        console.error(err);
                     })
             },
             {
                 title: `Checkout: "${submodule}:${developBranch}"`,
                 skip: ctx => ctx.abort,
                 task: ctx => execa.shell(`cd ${submodule} && git checkout ${developBranch}`)
-                    .catch((err) => {
+                    .catch(() => {
                         ctx.abort = true;
-                        console.error(err);
                     })
             },
             {
                 title: `Checkout "${submodule}:${developBranch}:${args.commitHash}"`,
                 skip: ctx => ctx.abort || args.commitHash === undefined || args.commitHash === null,
                 task: ctx => execa.shell(`cd ${submodule} && git checkout ${args.commitHash}`)
-                    .catch((err) => {
+                    .catch(() => {
                         ctx.abort = true;
-                        console.error(err);
                     })
             },
             {
                 title: `Create update submodule branch "${args.branch}" from "develop"`,
                 skip: ctx => ctx.abort || args.branch === undefined || args.branch === null,
                 task: ctx => execa.shell(`git checkout ${developBranch} && git branch -D ${args.branch} && git checkout -b ${args.branch}`)
-                    .catch((err) => {
+                    .catch(() => {
                         ctx.abort = true;
-                        console.error(err);
                     })
             },
             {
                 title: `Stage "${submodule}"`,
                 skip: ctx => ctx.abort,
                 task: ctx => execa.shell(`git add ${submodule}`)
-                    .catch((err) => {
+                    .catch(() => {
                         ctx.abort = true;
-                        console.error(err);
                     })
             },
             {
                 title: `Commit "${submodule}"`,
                 skip: ctx => ctx.abort,
                 task: ctx => execa.shell(`git commit -m "Update submodule ${submodule}"`)
-                    .catch((err) => {
+                    .catch(() => {
                         ctx.abort = true;
                     })
             },
@@ -70,10 +65,6 @@ program
                 title: `Pushing to remote`,
                 skip: ctx => ctx.abort,
                 task: ctx => execa.shell(`git push`)
-                    .catch((err) => {
-                        ctx.abort = true;
-                        console.error(err);
-                    })
             }
         ]).run().then(() => {
             execa.shell(`git show ${submodule}`)
--- dist/index.js
+++ dist/index.js
@@ -9,7 +9,7 @@ var commander_1 = __importDefault(require("commander"));
 var execa_1 = __importDefault(require("execa"));
 var listr_1 = __importDefault(require("listr"));
 commander_1.default
-    .version('0.5.1')
+    .version('0.5.2')
     .command('update <submodule>')
     .option('-C, --commitHash [commitHash]', 'commit id')
     .option('-b, --branch [updateSubmoduleBranch]', 'create update submodule branch from "develop" branch')
@@ -20,63 +20,54 @@ commander_1.default
             title: "Fetch ALL upstream",
             skip: function () { return false; },
             task: function (ctx) { return execa_1.default.shell("git fetch upstream --recurse-submodules --prune")
-                .catch(function (err) {
+                .catch(function () {
                 ctx.abort = true;
-                console.error(err);
             }); }
         },
         {
             title: "Checkout: \"" + submodule + ":" + developBranch + "\"",
             skip: function (ctx) { return ctx.abort; },
             task: function (ctx) { return execa_1.default.shell("cd " + submodule + " && git checkout " + developBranch)
-                .catch(function (err) {
+                .catch(function () {
                 ctx.abort = true;
-                console.error(err);
             }); }
         },
         {
             title: "Checkout \"" + submodule + ":" + developBranch + ":" + args.commitHash + "\"",
             skip: function (ctx) { return ctx.abort || args.commitHash === undefined || args.commitHash === null; },
             task: function (ctx) { return execa_1.default.shell("cd " + submodule + " && git checkout " + args.commitHash)
-                .catch(function (err) {
+                .catch(function () {
                 ctx.abort = true;
-                console.error(err);
             }); }
         },
         {
             title: "Create update submodule branch \"" + args.branch + "\" from \"develop\"",
             skip: function (ctx) { return ctx.abort || args.branch === undefined || args.branch === null; },
             task: function (ctx) { return execa_1.default.shell("git checkout " + developBranch + " && git branch -D " + args.branch + " && git checkout -b " + args.branch)
-                .catch(function (err) {
+                .catch(function () {
                 ctx.abort = true;
-                console.error(err);
             }); }
         },
         {
             title: "Stage \"" + submodule + "\"",
             skip: function (ctx) { return ctx.abort; },
             task: function (ctx) { return execa_1.default.shell("git add " + submodule)
-                .catch(function (err) {
+                .catch(function () {
                 ctx.abort = true;
-                console.error(err);
             }); }
         },
         {
             title: "Commit \"" + submodule + "\"",
             skip: function (ctx) { return ctx.abort; },
             task: function (ctx) { return execa_1.default.shell("git commit -m \"Update submodule " + submodule + "\"")
-                .catch(function (err) {
+                .catch(function () {
                 ctx.abort = true;
             }); }
         },
         {
             title: "Pushing to remote",
             skip: function (ctx) { return ctx.abort; },
-            task: function (ctx) { return execa_1.default.shell("git push")
-                .catch(function (err) {
-                ctx.abort = true;
-                console.error(err);
-            }); }
+            task: function (ctx) { return execa_1.default.shell("git push"); }
         }
     ]).run().then(function () {
         execa_1.default.shell("git show " + submodule)

63f491e82ca0231bd96acd90f2376b6f3e01627f
Khanh Dao <khanhdao.knd@gmail.com>
2018-06-25 14:25:12 +0700

    Fix bug

--- app/index.ts
+++ app/index.ts
@@ -13,36 +13,41 @@ program
 
         new Listr([
             {
-                title: `Checkout ${submodule}:${branch}`,
+                title: `Fetch ALL upstream`,
+                task: () => execa.shell(`git fetch upstream --recurse-submodules --prune`)
+            },
+            {
+                title: `Checkout: "${submodule}:${branch}"`,
                 task: () => execa.shell(`cd ${submodule} && git checkout ${branch}`)
             },
             {
-                title: `Fetch and rebase ${submodule}`,
-                task: () => execa.shell(`git submodule update ${submodule} --remote --rebase`)
-            },
-            {
-                title: `Checkout ${submodule}:${branch}:${args.commitHash}`,
+                title: `Checkout "${submodule}:${branch}:${args.commitHash}"`,
                 skip: () => args.commitHash === undefined || args.commitHash === null,
                 task: () => execa.shell(`cd ${submodule} && git checkout ${args.commitHash}`)
             },
             {
-                title: `Stage ${submodule}`,
+                title: `Stage "${submodule}"`,
                 task: () => execa.shell(`git add ${submodule}`)
             },
             {
-                title: `Commit ${submodule}`,
-                task: () => execa.shell(`git commit -m "Update submodule ${submodule}"`)
+                title: `Commit "${submodule}"`,
+                task: ctx => execa.shell(`git commit -m "Update submodule ${submodule}"`)
+                    .catch(() => {
+                        ctx.nothingToCommit = true;
+                    })
             },
             {
                 title: `Pushing to remote`,
+                skip: ctx => ctx.nothingToCommit,
                 task: () => execa.shell(`git push`)
-            },
-            {
-                title: `Commit of ${submodule} is now pointing to:`,
-                task: () => execa.shell(`git log --oneline -n 1 ${submodule}`)
-                    .then(result => { console.log(result.stdout); })
             }
-        ]).run().catch(error => {
+        ]).run().then(() => {
+            execa.shell(`git log --oneline -n 1 ${submodule}`)
+                .then(result => {
+                    console.log(`${submodule} is now pointing to:`);
+                    console.log(result.stdout);
+                });
+        }).catch(error => {
             console.error("Error:", error);
         });
     });
--- dist/index.js
+++ dist/index.js
@@ -16,36 +16,41 @@ commander_1.default
     var branch = 'develop';
     new listr_1.default([
         {
-            title: "Checkout " + submodule + ":" + branch,
+            title: "Fetch ALL upstream",
+            task: function () { return execa_1.default.shell("git fetch upstream --recurse-submodules --prune"); }
+        },
+        {
+            title: "Checkout: \"" + submodule + ":" + branch + "\"",
             task: function () { return execa_1.default.shell("cd " + submodule + " && git checkout " + branch); }
         },
         {
-            title: "Fetch and rebase " + submodule,
-            task: function () { return execa_1.default.shell("git submodule update " + submodule + " --remote --rebase"); }
-        },
-        {
-            title: "Checkout " + submodule + ":" + branch + ":" + args.commitHash,
+            title: "Checkout \"" + submodule + ":" + branch + ":" + args.commitHash + "\"",
             skip: function () { return args.commitHash === undefined || args.commitHash === null; },
             task: function () { return execa_1.default.shell("cd " + submodule + " && git checkout " + args.commitHash); }
         },
         {
-            title: "Stage " + submodule,
+            title: "Stage \"" + submodule + "\"",
             task: function () { return execa_1.default.shell("git add " + submodule); }
         },
         {
-            title: "Commit " + submodule,
-            task: function () { return execa_1.default.shell("git commit -m \"Update submodule " + submodule + "\""); }
+            title: "Commit \"" + submodule + "\"",
+            task: function (ctx) { return execa_1.default.shell("git commit -m \"Update submodule " + submodule + "\"")
+                .catch(function () {
+                ctx.nothingToCommit = true;
+            }); }
         },
         {
             title: "Pushing to remote",
+            skip: function (ctx) { return ctx.nothingToCommit; },
             task: function () { return execa_1.default.shell("git push"); }
-        },
-        {
-            title: "Commit of " + submodule + " is now pointing to:",
-            task: function () { return execa_1.default.shell("git log --oneline -n 1 " + submodule)
-                .then(function (result) { console.log(result.stdout); }); }
         }
-    ]).run().catch(function (error) {
+    ]).run().then(function () {
+        execa_1.default.shell("git log --oneline -n 1 " + submodule)
+            .then(function (result) {
+            console.log(submodule + " is now pointing to:");
+            console.log(result.stdout);
+        });
+    }).catch(function (error) {
         console.error("Error:", error);
     });
 });
